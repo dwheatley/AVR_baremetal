fb4816d1739f05130f67953f17c0748a43e01a16
Dylan Michael Wheatley <dylan.m.wheatley@gmail.com>
2016-01-02 01:23:48 -0800

    added signal debouncer

--- lesson1/button.c
+++ lesson1/button.c
@@ -1,5 +1,5 @@
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main() {
   /* set pin 4 of port b for input */
@@ -11,8 +11,12 @@ int main() {
   while(1) {
     /* test the bitwise value of pin 4*/
     if (bit_is_set(PINB, PINB4)) {
-      /* set pin 5 high if pin 4 is high */
-      PORTB |= _BV(PORTB5);
+      /* debounce the signal */
+      _delay_ms(25);
+      if (bit_is_set(PINB, PINB4)) {
+        /* valid signal, set pin 5 high if pin 4 is high */
+        PORTB |= _BV(PORTB5);
+      }
     }
     /* otherwise set pin 5 low */
     else PORTB &= ~_BV(PORTB5);
