f68ddc71924fad5e60c0df295898022a27cf649f
Dylan Michael Wheatley <dylan.m.wheatley@gmail.com>
2016-01-10 01:18:40 -0800

    define delay time

--- lesson2/flowing.c
+++ lesson2/flowing.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define DELAY 100
+
 int main() {
   /* set pins 2 through 9 on port d as output */
   DDRD |= (_BV(PORTD2) | _BV(PORTD3) | _BV(PORTD4) | _BV(PORTD5));
@@ -11,75 +13,75 @@ int main() {
   while(1) {
     /* A BLOCK, HIGH */
     PORTD |= _BV(PORTD2);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD3);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD4);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD5);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD6);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD7);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB |= _BV(PORTB0);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB |= _BV(PORTB1);
-    _delay_ms(100);
+    _delay_ms(DELAY);
 
     /* B BLOCK, LOW */
     PORTB &= ~_BV(PORTB1);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB &= ~_BV(PORTB0);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD7);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD6);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD5);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD4);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD3);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD2);
-    _delay_ms(100);
+    _delay_ms(DELAY);
 
     /* B BLOCK, HIGH */
     PORTB |= _BV(PORTB1);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB |= _BV(PORTB0);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD7);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD6);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD5);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD4);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD3);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD |= _BV(PORTD2);
-    _delay_ms(100);
+    _delay_ms(DELAY);
 
     /* A BLOCK, LOW */
     PORTD &= ~_BV(PORTD2);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD3);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD4);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD5);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD6);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTD &= ~_BV(PORTD7);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB &= ~_BV(PORTB0);
-    _delay_ms(100);
+    _delay_ms(DELAY);
     PORTB &= ~_BV(PORTB1);
-    _delay_ms(100);
+    _delay_ms(DELAY);
 
   }
   return 0;
