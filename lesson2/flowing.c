2e2b8533cfd312c128ac003bfd01b55d05f1010b
Dylan Michael Wheatley <dylan.m.wheatley@gmail.com>
2016-01-10 01:21:10 -0800

    correct port comment

--- lesson2/flowing.c
+++ lesson2/flowing.c
@@ -4,9 +4,9 @@
 #define DELAY 100
 
 int main() {
-  /* set pins 2 through 9 on port d as output */
-  DDRD |= (_BV(PORTD2) | _BV(PORTD3) | _BV(PORTD4) | _BV(PORTD5));
-  DDRD |= (_BV(PORTD6) | _BV(PORTD7));
+  /* set pins 2 through 7 on port d as output */
+  DDRD |= (_BV(PORTD2) | _BV(PORTD3) | _BV(PORTD4));
+  DDRD |= (_BV(PORTD5) | _BV(PORTD6) | _BV(PORTD7));
   /* set pins 0 and 1 on port b as output as well */
   DDRB |= (_BV(PORTB0) | _BV(PORTB1));
 
